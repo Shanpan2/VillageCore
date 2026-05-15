e5181d3fffb2f7703c00b106b5240bdd3680ee75
Shanpan2 <113867056+Shanpan2@users.noreply.github.com>
2026-05-15 20:11:26 +0900

    fix

--- bot_instance.py
+++ bot_instance.py
@@ -6,11 +6,13 @@ intents = discord.Intents.all()
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(
-            command_prefix="/",
+            command_prefix="!",  # スラッシュコマンドのみ使うので実質使わない文字に変更
             intents=intents,
-            application_id=1501521359963033741  # ← ここにあなたのアプリID
+            application_id=1501521359963033741
         )
 
+    async def on_message(self, message: discord.Message):
+        # prefix コマンドの処理（必須）
+        await self.process_commands(message)
+
 bot = MyBot()
-
-
